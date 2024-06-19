ca4ee8b279446f4d8fba5ab640c25c1e85283276
yhb131618 <119278327+yhb131618@users.noreply.github.com>
2024-06-19 14:10:32 +0900

    'add_auth_api'

---
+++ src/main/java/com/yellowstone/yellowboard/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.yellowstone.yellowboard.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.yellowstone.yellowboard.dto.response.ResponseDto;
+
+@RestControllerAdvice
+public class BadRequestExceptionHandler {
+    @ExceptionHandler({ MethodArgumentNotValidException.class, HttpMessageNotReadableException.class })
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception) {
+        return ResponseDto.validationFailed();
+    }
+}
